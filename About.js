758c840d894224c02fe8d34465703a3763c1ba33
arunaananthagiri <127080467+arunaananthagiri@users.noreply.github.com>
2024-08-26 21:19:21 +0530

    components

---
+++ About.js
@@ -0,0 +1,16 @@
+// src/components/About.js
+import React from 'react';
+import './About.css';
+
+const About = () => {
+  return (
+    <section id="about">
+      <h2>About China Tourism</h2>
+      <p>
+        Explore the rich history, stunning landscapes, and vibrant culture of China. From the majestic Great Wall to the serene Li River, and bustling cities like Beijing and Shanghai, China offers an unforgettable journey. Discover ancient temples, modern marvels, and diverse culinary delights in this fascinating and dynamic country.
+      </p>
+    </section>
+  );
+};
+
+export default About;
