14060ffb1a70db0cc4f63ae6d190ae65d48773fb
arunaananthagiri <127080467+arunaananthagiri@users.noreply.github.com>
2024-08-28 23:16:22 +0530

    components

---
+++ Contact.js
@@ -0,0 +1,18 @@
+// src/components/Contact.js
+import React from 'react';
+import './Contact.css';
+
+const Contact = () => {
+  return (
+    <section id="contact" className="contact">
+      <h2>Contact Us</h2>
+      <ul>
+        <li>Phone: 123-456-7890</li>
+        <li>Email: travelguide@example.com</li>
+        <li>Instagram: @china_places</li>
+      </ul>
+    </section>
+  );
+};
+
+export default Contact;
